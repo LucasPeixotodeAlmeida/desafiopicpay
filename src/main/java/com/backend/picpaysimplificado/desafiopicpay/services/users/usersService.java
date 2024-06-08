ec2fc20e030501b0c3754df7654f3d922cf18425
Lucas Peixoto <lucaspeixoto349@gmail.com>
2024-06-08 19:53:35 -0300

    criação createUser e List

--- src/main/java/com/backend/picpaysimplificado/desafiopicpay/services/users/usersService.java
+++ src/main/java/com/backend/picpaysimplificado/desafiopicpay/services/users/usersService.java
@@ -1,6 +1,7 @@
 package com.backend.picpaysimplificado.desafiopicpay.services.users;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import com.backend.picpaysimplificado.desafiopicpay.domain.users.entity.UsersEntity;
 import com.backend.picpaysimplificado.desafiopicpay.domain.users.enums.UserTypeEnum;
+import com.backend.picpaysimplificado.desafiopicpay.dto.users.UsersDTO;
 import com.backend.picpaysimplificado.desafiopicpay.repositories.users.UsersRepository;
 
 @Service
@@ -27,6 +29,16 @@ public class UsersService {
 
     public UsersEntity findUsersById(UUID id) throws Exception{
         return this.usersRepository.findUsersById(id).orElseThrow(() -> new Exception("Usuário não encontrado"));
+    }
+
+    public UsersEntity createUser(UsersDTO data){
+        UsersEntity newUser = new UsersEntity(data);
+        this.saveUsers(newUser);
+        return newUser;
+    }
+
+    public List<UsersEntity> getAllUsers(){
+        return this.usersRepository.findAll();
     }
 
     public void saveUsers(UsersEntity user){
