c1f151c365d1a6e4f6d1337cdbeb67f6bee23fa2
Lucas Peixoto <lucaspeixoto349@gmail.com>
2024-06-08 17:26:37 -0300

    criação transactionEntity

---
+++ src/main/java/com/backend/picpaysimplificado/desafiopicpay/domain/transaction/entity/TransactionEntity.java
@@ -0,0 +1,45 @@
+package com.backend.picpaysimplificado.desafiopicpay.domain.transaction.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.backend.picpaysimplificado.desafiopicpay.domain.users.entity.UsersEntities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "transactions")
+@Table(name = "transactions")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TransactionEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private UsersEntities sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private UsersEntities receiver;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
